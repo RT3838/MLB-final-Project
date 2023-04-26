c2a4304b6ee610b9187421d92ff21812ac4d04d4
RT3838 <118489609+RT3838@users.noreply.github.com>
2023-04-26 18:53:19 -0400

    Newest addition
    hi

---
+++ MLBTeamSchedule.java
@@ -0,0 +1,45 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import com.google.gson.Gson;
+
+     
+     
+      public class MLBTeamSchedule {
+         
+         
+          public static void main(String[] args) throws Exception {
+       
+        HttpRequest request = HttpRequest.newBuilder()
+                
+                .uri(URI.create("https://tank01-mlb-live-in-game-real-time-statistics.p.rapidapi.com/getMLBTeamSchedule?teamAbv=CHW"))
+               
+                .header("X-RapidAPI-Key", "948121a9b1mshe3f3de56ea683cap17e72djsncf4dc795506f")
+               
+                .header("X-RapidAPI-Host", "tank01-mlb-live-in-game-real-time-statistics.p.rapidapi.com")
+               
+                .method("GET", HttpRequest.BodyPublishers.noBody())
+               
+               
+                .build();
+
+
+        HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+
+       
+        String responseBody = response.body();
+
+       
+      
+        Gson gson = new Gson();
+       
+        MLBTeamScheduleResponse mlbTeamScheduleResponse = gson.fromJson(responseBody, MLBTeamScheduleResponse.class);
+
+        
+       
+        System.out.println(mlbTeamScheduleResponse);
+    
+    }
+    
+}
